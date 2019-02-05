12b1f0dd1498604592f431cd2badf8dedee6fbac
s1191 <danieljudsonblair@gmail.com>
2019-02-05 16:47:35 -0500

    updated schema and seeds sql files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
-INSERT INTO burgers (name, eaten) VALUES ('Double CheeseBurger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Single CheeseBurger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Double CheeseBurger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Triple CheeseBurger', false);
