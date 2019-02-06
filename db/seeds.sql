eaa915fbd1b5f227e29ea660c96565f578dfe1c2
s1191 <danieljudsonblair@gmail.com>
2019-02-05 20:15:10 -0500

    small changes.. still broken

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (name, devoured) VALUES ('Single CheeseBurger', false);
-INSERT INTO burgers (name, devoured) VALUES ('Double CheeseBurger', false);
-INSERT INTO burgers (name, devoured) VALUES ('Triple CheeseBurger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Single CheeseBurger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double CheeseBurger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple CheeseBurger', false);
